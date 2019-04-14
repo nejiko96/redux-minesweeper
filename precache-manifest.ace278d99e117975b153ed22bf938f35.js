3c262992a74ccbf0f2e9edc2ccf49a156cf962d4
nejiko96 <nejiko2006@gmail.com>
2019-04-15 00:44:56 +0900

    Updates

---
+++ precache-manifest.ace278d99e117975b153ed22bf938f35.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "75ae2369272ee13f3ffc",
+    "url": "/redux-minesweeper/static/js/main.99cc94bd.chunk.js"
+  },
+  {
+    "revision": "f113722d268e3e5e4122",
+    "url": "/redux-minesweeper/static/js/runtime~main.babce75d.js"
+  },
+  {
+    "revision": "b43c83579e1f2bdac463",
+    "url": "/redux-minesweeper/static/js/2.634d42ad.chunk.js"
+  },
+  {
+    "revision": "12fcab7a4cd75bc3f045d18762df4cca",
+    "url": "/redux-minesweeper/index.html"
+  }
+];
