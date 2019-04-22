82c51f15a1ddf248aec3bf5770b65f3ff68427e9
nejiko96 <nejiko2006@gmail.com>
2019-04-22 11:46:20 +0900

    separate Header from App

--- src/App.js
+++ src/App.js
@@ -2,13 +2,7 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Tooltip from '@material-ui/core/Tooltip';
-import IconButton from '@material-ui/core/IconButton';
-import GithubCircle from 'mdi-material-ui/GithubCircle';
+import Header from './Header';
 
 import reducer from './reducers';
 import subscribeAll from './listeners';
@@ -17,35 +11,11 @@ import MineSweeperContainer from './containers/MineSweeperContainer';
 const store = createStore(reducer);
 subscribeAll(store);
 
-const styles = {
-  root: {
-    flexGrow: 1,
-  },
-  grow: {
-    flexGrow: 1,
-  },
-};
-
 class App extends Component {
   render() {
-    const { classes } = this.props;
     return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6" color="inherit" className={classes.grow}>
-              redux-minesweeper demo
-            </Typography>
-            <Tooltip title="View source on Github" aria-label="View source on Github">
-              <IconButton
-                color="inherit"
-                aria-label="Github"
-                href="https://github.com/nejiko96/redux-minesweeper">
-                <GithubCircle />
-              </IconButton>
-            </Tooltip>
-          </Toolbar>
-        </AppBar>
+      <div>
+        <Header />
         <Provider store={store}>
           <MineSweeperContainer />
         </Provider>
@@ -54,4 +24,4 @@ class App extends Component {
   }
 }
 
-export default withStyles(styles)(App);
+export default App;
