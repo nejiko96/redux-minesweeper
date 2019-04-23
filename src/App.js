1820be405d3226e0b38a6b31bb3a2b76738d38f7
nejiko96 <nejiko2006@gmail.com>
2019-04-23 13:53:04 +0900

    use CssBaseline

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import Header from './Header';
 
@@ -14,12 +15,13 @@ subscribeAll(store);
 class App extends Component {
   render() {
     return (
-      <div>
+      <React.Fragment>
+        <CssBaseline />
         <Header />
         <Provider store={store}>
           <MineSweeperContainer />
         </Provider>
-      </div>
+      </React.Fragment>
     );
   }
 }
